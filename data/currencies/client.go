bf8511cb204077c7634226dfc371ceb7bd9c9592
Tommy Chu <tommychu2256@gmail.com>
2020-08-02 10:06:58 +0200

    add Init and Close methods for the CurrencyService

--- data/currencies/client.go
+++ data/currencies/client.go
@@ -2,10 +2,11 @@ package data
 
 import (
 	currency "github.com/chutified/currencies/protos/currency"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
-// CurrencyServicE allow the rpc calls. The service provides currency data.
+// CurrencyService allow the rpc calls. The service provides currency data.
 type CurrencyService struct {
 	client currency.CurrencyClient
 	conn   *grpc.ClientConn
@@ -14,4 +15,23 @@ type CurrencyService struct {
 // NewCurrency is the CurrencyService constructor.
 func NewCurrency() *CurrencyService {
 	return &CurrencyService{}
+}
+
+func (cs *CurrencyService) Init(target string) error {
+
+	// get connection
+	conn, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		return errors.Wrap(err, "unable to dial ")
+	}
+
+	// register client
+	cs.client = currency.NewCurrencyClient(conn)
+	cs.conn = conn
+	return nil
+}
+
+// Close cancels the connection between the client and the server.
+func (cs *CurrencyService) Close() error {
+	return cs.conn.Close()
 }
