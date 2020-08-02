2b2936b960b8d35928563ef2b6f5aa99165b3283
Tommy Chu <tommychu2256@gmail.com>
2020-08-02 09:24:35 +0200

    define Init and Close mothods on CryptoService

--- data/cryptos/client.go
+++ data/cryptos/client.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	crypto "github.com/chutified/crypto-currencies/protos/crypto"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
@@ -14,4 +15,24 @@ type CryptoService struct {
 // NewCrypto is the CryptoService constructor.
 func NewCrypto() *CryptoService {
 	return &CryptoService{}
+}
+
+// Init starts the server connection and enables rpc calls.
+func (cs *CryptoService) Init(target string) error {
+
+	// get connection
+	conn, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		return errors.Wrap(err, "unable to dial commodity service")
+	}
+
+	// register client
+	cs.client = crypto.NewCryptoClient(conn)
+	cs.conn = conn
+	return nil
+}
+
+// Close cancels the connection between the client and the server.
+func (cs *CryptoService) Close() error {
+	return cs.conn.Close()
 }
