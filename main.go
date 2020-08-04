0272c48b7c368789d10cee69947f06d8a142a222
Tommy Chu <tommychu2256@gmail.com>
2020-08-04 20:42:49 +0200

    add main func

--- main.go
+++ main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	config "github.com/chutified/market-info/config"
+	server "github.com/chutified/market-info/server"
+)
+
 func main() {
+	log := log.New(os.Stdout, "[ERROR]", log.LstdFlags)
+
+	// get config
+	cfg, err := config.GetConfig("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// server
+	srv := server.New()
+	err = srv.Set(cfg)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer func() {
+		log.Fatal(srv.Stop())
+	}()
+	// run
+	log.Panic(srv.Start())
 }
