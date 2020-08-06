c1124c891097cac790ed1c6822755106b2fd069d
Tommy Chu <tommychu2256@gmail.com>
2020-08-06 12:54:14 +0200

    todo

--- handlers/commodities.go
+++ handlers/commodities.go
@@ -2,6 +2,14 @@ package handlers
 
 import "github.com/gin-gonic/gin"
 
+// GetCommity serves the commodity data to the client.
 func (h *Handler) GetCommodity(c *gin.Context) {
+
+	// get requested the commodity
+	name := c.Param("name")
+	cmd, err := h.commoditySrv.GetCommodity(name)
+	if err != nil {
+		c.JSON() // TODO
+	}
 
 }
