a02e4156093fc9ba9fdd6bd65531a35d378a7ad3
Tommy Chu <tommychu2256@gmail.com>
2020-08-11 21:24:38 +0200

    rename the config file from config.yaml to config.yml

--- main.go
+++ main.go
@@ -9,23 +9,27 @@ import (
 )
 
 func main() {
-	log := log.New(os.Stdout, "[ERROR]", log.LstdFlags)
+	log := log.New(os.Stdout, "[STATUS] ", log.LstdFlags)
 
 	// get config
-	cfg, err := config.GetConfig("config.yaml")
+	log.Printf("Getting config file...")
+	cfg, err := config.GetConfig("config.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// server
+	log.Printf("Setting server...")
 	srv := server.New()
 	err = srv.Set(cfg)
 	if err != nil {
 		log.Panic(err)
 	}
 	defer func() {
+		log.Printf("Stopping server...")
 		log.Fatal(srv.Stop())
 	}()
 	// run
+	log.Printf("Server is running on port %d.", cfg.APIPort)
 	log.Panic(srv.Start())
 }
