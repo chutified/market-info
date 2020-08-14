7fc9ab5bc125cc7117bbee88c560f5bebae40641
Tommy Chu <tommychu2256@gmail.com>
2020-08-14 19:46:13 +0200

    complete the GetCrypto handler

--- handlers/cryptos.go
+++ handlers/cryptos.go
@@ -2,6 +2,15 @@ package handlers
 
 import "github.com/gin-gonic/gin"
 
+// GetCrypto handles the GetCrypto request and response with the
+// latest available data about the given cryptocurrency.
 func (h *Handler) GetCrypto(c *gin.Context) {
-	// TODO
+
+	name := c.Param("name")
+	crp, err := h.cryptoSrv.GetCrypto(name)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(200, crp)
 }
--- handlers/currencies.go
+++ handlers/currencies.go
@@ -3,9 +3,20 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func (h *Handler) GetCurrency(c *gin.Context) {
-	// TODO
+
+	name := c.Params("name")
 }
 
 func (h *Handler) GetRate(c *gin.Context) {
-	// TODO
+
+	base := c.Params("base")
+	dest := c.Params("dest")
+
+	// calculate the rate
+	rate, err := h.cryptoSrv.GetRate(base, dest)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(200, rate)
 }
